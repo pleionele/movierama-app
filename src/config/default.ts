7f27b99a74b58a90f68fb67628e82460eabf6ea2
pleionele <ep_di@hotmail.com>
2020-01-25 19:57:41 +0000

    added url for view more details section

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,5 +3,8 @@ export enum Config {
   PlayNowUrl = '/movie/now_playing',
   GenresListUrl = '/genre/movie/list',
   SearchUrl = '/search/movie',
+  VideoUrl = '/movie/{movie_id}/videos',
+  ReviewsUrl = '/movie/{movie_id}/reviews',
+  SimilarUrl = '/movie/{movie_id}/similar',
   ApiKey = 'bc50218d91157b1ba4f142ef7baaa6a0',
 }
