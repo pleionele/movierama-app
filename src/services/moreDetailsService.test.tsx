d1656d8b07348fb8a144c095f242af0cec5dd1a4
pleionele <ep_di@hotmail.com>
2020-01-26 12:53:37 +0000

    added payloads for the more details section

--- src/services/moreDetailsService.test.tsx
+++ src/services/moreDetailsService.test.tsx
@@ -3,7 +3,11 @@ import { getMovieReviews } from '../api/get-movie-reviews';
 import { getMovieSimilar } from '../api/get-movie-similar';
 import { getMovieVideo } from '../api/get-movie-video';
 // @ts-ignore
-import * as payloadPayNowMovie from '../api/stubs/payload-pay-now-movie.json';
+import * as payloadMovieReviews from '../api/stubs/payload-movie-reviews.json';
+// @ts-ignore
+import * as payloadSimilarMovies from '../api/stubs/payload-similar-movies.json';
+// @ts-ignore
+import * as payloadMovieVideo from '../api/stubs/payload-movie-video.json';
 
 jest.mock('../api/get-movie-reviews', () => {
   return {
@@ -24,13 +28,18 @@ jest.mock('../api/get-movie-video', () => {
 
 describe('playNowMovieService', () => {
   it('should make the correct api calls', () => {
-    (getMovieVideo as jest.Mock).mockReturnValue(payloadPayNowMovie);
-    (getMovieSimilar as jest.Mock).mockReturnValue(payloadPayNowMovie);
-    (getMovieReviews as jest.Mock).mockReturnValue(payloadPayNowMovie);
+    (getMovieVideo as jest.Mock).mockReturnValue(payloadMovieVideo);
+    (getMovieSimilar as jest.Mock).mockReturnValue(payloadSimilarMovies);
+    (getMovieReviews as jest.Mock).mockReturnValue(payloadMovieReviews);
 
-    moreDetailsService('1212');
+    moreDetailsService(1212);
     expect(getMovieVideo).toHaveBeenCalledTimes(1);
+    expect(getMovieVideo).toHaveBeenCalledWith('1212');
+
     expect(getMovieReviews).toHaveBeenCalledTimes(1);
+    expect(getMovieReviews).toHaveBeenCalledWith('1212');
+
     expect(getMovieSimilar).toHaveBeenCalledTimes(1);
+    expect(getMovieSimilar).toHaveBeenCalledWith('1212');
   });
 });
--- src/services/moreDetailsService.tsx
+++ src/services/moreDetailsService.tsx
@@ -4,13 +4,11 @@ import { getMovieVideo } from '../api/get-movie-video';
 
 export const moreDetailsService = (id: number) => {
   const idToString = id.toString();
-
   const reviews: any = getMovieReviews(idToString);
   const similarMovies: any = getMovieSimilar(idToString);
   const video: any = getMovieVideo(idToString);
 
   return Promise.all([reviews, similarMovies, video]).then(results => {
-    console.log(results);
     return Object.assign(results[0], results[1], results[2]);
   });
 };
