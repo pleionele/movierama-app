3d14cb957258a4f96baef5faefa17a45683c4a76
pleionele <ep_di@hotmail.com>
2020-01-26 01:22:18 +0000

    removed babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.tsx'],
+  entry: ['./src/index.tsx'],
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist',
@@ -16,7 +16,12 @@ module.exports = {
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.tsx?$/, loader: 'babel-loader' },
       { test: /\.tsx?$/, loader: 'ts-loader' },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        exclude: /node_modules/,
+      },
     ],
   },
   plugins: [
